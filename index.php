8a1d4951582fcc8e492c4ba43d69ca83bf08421a
Florian Puig <flopuig98@gmail.com>
2022-03-08 17:46:35 +0100

    login et vérification 2

--- index.php
+++ index.php
@@ -5,21 +5,30 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>KGB</title>
-        <!-- importer le fichier de style -->
+        <link rel="icon" href="image/favicon.ico" type="image/x-icon" />
         <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
     </head>
-    <body style='background:#fff;'>
-        <div id="content">
-            <!-- tester si l'utilisateur est connecté -->
+<body>
+    <div id="container">
+            
+        <form action="verification.php" method="POST">
+            <h1>Connexion</h1>
+                
+            <label><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+            <label><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+            <input type="submit" id='submit' value='LOGIN' >
             <?php
-                session_start();
-                if($_SESSION['username'] !== ""){
-                    $user = $_SESSION['username'];
-                    // afficher un message
-                    echo "Bonjour $user, vous êtes connecté";
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
                 }
             ?>
-            
-        </div>
-    </body>
+        </form>
+    </div>   
+</body>
 </html>
---
+++ principal.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>KGB</title>
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body style='background:#fff;'>
+        <div id="content">
+            <!-- tester si l'utilisateur est connecté -->
+            <?php
+                session_start();
+                if($_SESSION['username'] !== ""){
+                    $user = $_SESSION['username'];
+                    // afficher un message
+                    echo "Bonjour $user, vous êtes connecté";
+                }
+            ?>
+            
+        </div>
+    </body>
+</html>
