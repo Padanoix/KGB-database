76d3446cfc794ee78271690f4fad8cc77a15545f
Florian Puig <flopuig98@gmail.com>
2022-03-08 17:44:56 +0100

    login et vérification

--- index.php
+++ index.php
@@ -5,30 +5,21 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>KGB</title>
-        <link rel="icon" href="image/favicon.ico" type="image/x-icon" />
+        <!-- importer le fichier de style -->
         <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
     </head>
-<body>
-    <div id="container">
-            
-        <form action="verification.php" method="POST">
-            <h1>Connexion</h1>
-                
-            <label><b>Nom d'utilisateur</b></label>
-            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-
-            <label><b>Mot de passe</b></label>
-            <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
-            <input type="submit" id='submit' value='LOGIN' >
+    <body style='background:#fff;'>
+        <div id="content">
+            <!-- tester si l'utilisateur est connecté -->
             <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                session_start();
+                if($_SESSION['username'] !== ""){
+                    $user = $_SESSION['username'];
+                    // afficher un message
+                    echo "Bonjour $user, vous êtes connecté";
                 }
             ?>
-        </form>
-    </div>   
-</body>
+            
+        </div>
+    </body>
 </html>
